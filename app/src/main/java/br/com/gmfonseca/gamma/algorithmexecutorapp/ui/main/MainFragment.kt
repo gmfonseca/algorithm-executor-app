331af2177edbcca185d2562f3ec9805c7bc8b534
gmfonseca <gmagnofonseca@gmail.com>
2021-02-28 14:46:57 -0300

    Add main screen layout

--- app/src/main/java/br/com/gmfonseca/gamma/algorithmexecutorapp/ui/main/MainFragment.kt
+++ app/src/main/java/br/com/gmfonseca/gamma/algorithmexecutorapp/ui/main/MainFragment.kt
@@ -1,12 +1,13 @@
 package br.com.gmfonseca.gamma.algorithmexecutorapp.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import br.com.gmfonseca.gamma.algorithmexecutorapp.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -16,15 +17,20 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = defaultViewModelProviderFactory.create(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        button_start.setOnClickListener {
+            Toast.makeText(context, "Clicked :)", Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/br/com/gmfonseca/gamma/algorithmexecutorapp/ui/main/MainViewModel.kt
+++ app/src/main/java/br/com/gmfonseca/gamma/algorithmexecutorapp/ui/main/MainViewModel.kt
@@ -3,5 +3,5 @@ package br.com.gmfonseca.gamma.algorithmexecutorapp.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
